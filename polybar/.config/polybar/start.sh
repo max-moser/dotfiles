45b736a00638e85642a7dd0a2349164ae42bf8c6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2020-12-14 22:29:14 +0100

    Add option to kill old polybar instances

--- polybar/.config/polybar/start.sh
+++ polybar/.config/polybar/start.sh
@@ -1,12 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+
+kill_polybar=0
+bar=max
+while getopts "kb:" opt; do
+	case $opt in
+		k)
+			kill_polybar=1
+			;;
+		b)
+			bar=$OPTARG
+			;;
+		*)
+			echo "ignored unknown option: $opt" >&2
+			;;
+	esac
+done
 
 if [[ -z $(pgrep -x polybar) ]]; then
-	BAR=max
-
 	for m in $(polybar --list-monitors | cut -d ":" -f 1); do
-		MONITOR=$m polybar --reload $BAR &
+		monitor=$m polybar --reload $bar &
 		sleep 0.1
 	done
+
+elif [[ $kill_polybar -ge 1 ]]; then
+	killall polybar
+	sleep 0.5
+	exec $0 -b $bar
+
 else
 	polybar-msg cmd restart
 fi
