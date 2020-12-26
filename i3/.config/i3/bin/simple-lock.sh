5044704b6ab07dbce91156d37f4d4e7c628f37fd
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2020-12-26 01:36:45 +0100

    Add option for using blurred screenshot as lock screen
    * Note: this is significantly slower than just using a colour

--- i3/.config/i3/bin/simple-lock.sh
+++ i3/.config/i3/bin/simple-lock.sh
@@ -2,7 +2,9 @@
 
 timeout=60
 colour=000000
-while getopts "t:c:" opt; do
+screenshot=0
+image_file=
+while getopts "t:c:i:s" opt; do
 	case $opt in
 		t)
 			timeout=$OPTARG
@@ -18,6 +20,16 @@ while getopts "t:c:" opt; do
 			   exit 1
 			fi
 			;;
+		s)
+			screenshot=1
+			;;
+		i)
+			image_file=$OPTARG
+			if [[ ! -f $image_file ]]; then
+				echo "error: image file $image_file does not exist" >&2
+				exit 1
+			fi
+			;;
 		*)
 			echo "ignoring unknown option: $opt" >&2
 			;;
@@ -25,17 +37,40 @@ while getopts "t:c:" opt; do
 done
 
 if [[ ! -f $HOME/.no-lock ]]; then
-	i3lock -p default -c $colour
-	locked=1
-	while [[ $locked -gt 0 ]]; do
-		sleep $timeout
-		pgrep i3lock
-		if [[ $? -eq 0 ]]; then
-			xset dpms force off
-			sleep 1
-		else
-			locked=0
-		fi
-	done
+	screenshot_file=
+	if [[ $screenshot -gt 0 ]]; then
+		# create screenshot and blur it
+		# (the downscaling is done to improve blurring speed)
+		umask 077
+		screenshot_file=$(mktemp --suff='.png')
+		image_file=$screenshot_file
+		scrot -z -o $screenshot_file
+		convert $screenshot_file -resize "12.5%" $screenshot_file
+		convert $screenshot_file -blur 0x3 $screenshot_file
+		convert $screenshot_file -resize "800%" $screenshot_file
+	fi
+
+	if [[ -f $image_file ]]; then
+		i3lock -p default -c $colour -i $image_file
+	else
+		i3lock -p default -c $colour
+	fi
+
+	# if we created a screenshot, delete it again
+	[[ -f $screenshot_file ]] && shred -u $screenshot_file
+
+	if [[ $timeout -gt 0 ]]; then
+		locked=1
+		while [[ $locked -gt 0 ]]; do
+			sleep ${timeout}s
+			pgrep i3lock
+			if [[ $? -eq 0 ]]; then
+				xset dpms force off
+				sleep 1
+			else
+				locked=0
+			fi
+		done
+	fi
 fi
 
