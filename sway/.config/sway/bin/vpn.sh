29beb3cd9c7e260d19a1067052ad860e069c98e6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-07-29 22:12:46 +0200

    sway/vpn: enhance functionality
    * support multiple active VPNs at the same time
    * add options for focing 'connect' or 'disconnect' action
    * if no flags are specified, pick the action automatically
    * add new mode_vpn in sway via ctrl+shift+alt+v

--- sway/.config/sway/bin/vpn.sh
+++ sway/.config/sway/bin/vpn.sh
@@ -1,33 +1,81 @@
 #!/bin/bash
+#
+# vim: noexpandtab
+#
+# script for (de-)activating a VPN connection via the NetworkManager CLI
+# after querying the user which VPN to connect to/disconnect from via rofi
+#
+# an assumption is that a user usually wants to connect only to one VPN at a time
 
-active_vpn=
+active_vpns=()
 available_vpns=()
-has_available_vpns=0
+connect=1
+force_disconnect=0
+force_connect=0
 
+# parse arguments
+while getopts "cdh" opt; do
+	case "${opt}" in
+		c)
+			force_connect=1
+			;;
+		d)
+			force_disconnect=1
+			;;
+		h)
+			echo -e "usage: ${0} [OPTION...]\n"
+			echo "  -c    force 'connect' action"
+			echo "  -d    force 'disconnect' action"
+			echo "  -h    show this help screen and exit"
+			exit
+			;;
+		*)
+			echo "ignoring unknown option: ${opt}" >&2
+			;;
+	esac
+done
+
+if [[ "${force_connect}" -gt 0 && "${force_disconnect}" -gt 0 ]]; then
+	echo "error: cannot force connect and disconnect at the same time" >&2
+	exit 1
+fi
+
+# query NetworkManager for available/active VPNs
 while read -r line; do
 	state=$(echo "$line" | awk '{ print $2 }')
 	name=$(echo "$line" | awk '{ $1=$2=""; print substr($0, 3) }')
 
 	if [[ "$state" =~ activated ]]; then
-		active_vpn="$name"
+		active_vpns+=( "$name" )
+		connect=0
 	else
 		available_vpns+=( "$name" )
-		has_available_vpns=1
 	fi
 done < <(nmcli --fields=TYPE,STATE,NAME connection show | grep '^vpn\W')
 
-if [[ -n "$active_vpn" ]]; then
-	# TODO make script more flexible (allow other dmenu replacements than just rofi)
-	selection=$(echo -e "disconnect from '$active_vpn'\ncancel" | rofi -dmenu)
-	if [[ ! "$selection" =~ cancel ]]; then
-		nmcli connection down "$active_vpn"
+# override the action if forced
+if [[ "${force_connect}" -gt 0 ]]; then
+	connect=1
+elif [[ "${force_disconnect}" -gt 0 ]]; then
+	connect=0
+fi
+
+# perform the selected action
+# if an action was forced with no options, the selection will be empty... which is okay
+if [[ "${connect}" -eq 0 ]]; then
+	if selection=$(for vpn in "${active_vpns[@]}"; do echo "${vpn}"; done | rofi -dmenu -p "Disconnect from VPN"); then
+		if [[ -n "${selection}" ]]; then
+			nmcli connection down "$selection"
+		fi
 	fi
 
-elif [[ $has_available_vpns -gt 0 ]]; then
-	# TODO same as above
-	selection=$(for vpn in "${available_vpns[@]}"; do echo $vpn; done | rofi -dmenu)
-	nmcli connection up "$selection"
+elif [[ "${connect}" -gt 0 ]]; then
+	if selection=$(for vpn in "${available_vpns[@]}"; do echo "${vpn}"; done | rofi -dmenu -p "Connect to VPN"); then
+		if [[ -n "${selection}" ]]; then
+			nmcli connection up "$selection"
+		fi
+	fi
 
 else
-	echo "no vpns available." >&2
+	echo "No VPNs available." >&2
 fi
