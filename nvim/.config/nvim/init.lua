ef652fa423dac831305fcee4bc3e5194c2d6bc8b
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-07-28 17:01:15 +0200

    nvim: automatically save/restore sessions for projects
    * if the only argument for neovim during startup is a directory, it
      assumes that's a project directory and enables automatic saving and
      restoring of sessions for this directory
    * this makes it very easy to continue where you left off the last time,
      and simulates the behaviour of some IDEs
    * the sessions are stored in $XDG_DATA_HOME/nvim/sessions/

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- workaround for the "undefined global `vim`" warning all over the place
+local vim = vim
+
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -26,5 +29,42 @@ require("config.autocommands")
 -- for projects: if the only argument to vim is a directory, let's go there
 local args = vim.fn.argv()
 if #args == 1 and vim.fn.isdirectory(args[1]) == 1 then
-    vim.cmd.cd(args[1])
+    local project_dir = args[1]
+    vim.cmd.cd(project_dir)
+
+    -- store the session file in the XDG "data" path (usually `~/.local/share/...`)
+    local session_data_dir = vim.fn.stdpath("data") .. "/sessions/"
+    local cwd = vim.loop.cwd()
+    vim.fn.mkdir(session_data_dir, "p")
+    local session_file = session_data_dir .. cwd:gsub("/", "_")
+
+    -- if we have a session file available for the project, restore the session
+    if vim.fn.filereadable(session_file) == 1 then
+        -- close the buffer that's open before restoring the session
+        local cur_buf = vim.api.nvim_get_current_buf()
+        vim.cmd.source(session_file)
+        vim.cmd.bdelete({cur_buf, bang = true })
+
+        -- sometimes it happens that there's another open buffer for the project dir
+        local buffers = vim.api.nvim_list_bufs()
+        if #buffers > 1 then
+            for _, buf in pairs(buffers) do
+                if vim.api.nvim_buf_get_name(buf) == cwd then
+                    vim.cmd.bdelete({project_dir, bang = true})
+                end
+            end
+        end
+    end
+
+    -- just before quitting vim, save the current session
+    vim.api.nvim_create_autocmd("VimLeave", {
+        group = vim.api.nvim_create_augroup("project_session", { clear = true }),
+        callback = function()
+            if vim.api.nvim_get_vvar("dying") == 0 then
+                -- only save the session if vim is being quit normally
+                vim.cmd.mksession({session_file, bang = true })
+            end
+        end,
+
+    })
 end
