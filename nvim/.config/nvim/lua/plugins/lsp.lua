1d0510514ef179c03325e28a2ec90f8415bb08b2
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2024-03-10 17:28:24 +0100

    nvim: configure completion and snippets
    * `C-{u,d,j,k}` for scrolling the docs during completion
    * `Enter` simply accepts the completion suggestion
    * `Tab` accepts the suggestion or jumps to the next snippet node
    * `S-Tab` jumps to the previous snippet node

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
         {"neovim/nvim-lspconfig"},
         {"folke/neodev.nvim"},
 
-        -- mason for installing new LSP servers
+        -- mason for installing new language servers
         {
             "williamboman/mason.nvim",
             build = function ()
@@ -34,29 +34,72 @@ return {
             map("i", "<C-S-Space>", vim.lsp.buf.signature_help, "Signature Help")
         end)
 
-        -- `neodev` takes care of setting up the Lua LS for neovim,
-        -- needs to be set up before `lspconfig`
+        -- `neodev` takes care of setting up the Lua LS for neovim, needs to be set up before `lspconfig`
         require("neodev").setup({})
+        local lsp_zero_server = require("lsp-zero.server")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
         require("mason").setup({})
         require("mason-lspconfig").setup({
             ensure_installed = {},
-            handlers = { lsp_zero.default_setup }
+            handlers = {
+                function(lsp_name)
+                    -- slightly extended `lsp_zero.default_setup(lsp_name)`
+                    lsp_zero_server.setup(lsp_name, {
+                        capabilities = cmp_nvim_lsp.default_capabilities(),
+                    })
+                end,
+            },
         })
 
         -- `cmp` needs to be set up after `lsp-zero`
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
         cmp.setup({
-            mapping = {
-                -- allow `Enter` and `Tab` to confirm completion
-                ["<CR>"] = cmp.mapping.confirm({select = false, behavior = cmp.ConfirmBehavior.Replace}),
-                ["<Tab>"] = cmp.mapping.confirm({select = false, behavior = cmp.ConfirmBehavior.Replace}),
-
+            enabled = true,
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
                 -- `Ctrl-Space` to trigger completion menu
                 ["<C-Space>"] = cmp.mapping.complete(),
-            },
+
+                -- scrolling the docs
+                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                ["<C-k>"] = cmp.mapping.scroll_docs(-1),
+                ["<C-j>"] = cmp.mapping.scroll_docs(1),
+
+                -- allow `Enter` and `Tab` to confirm completion,
+                -- but also use `Tab` and `Shift-Tab` for cycling through suggestions
+                ["<CR>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        -- if a suggestion is currently open, i want to accept it with `Tab`
+                        cmp.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace })
+                    elseif luasnip.expand_or_jumpable() then
+                        -- if i'm still in a snippet, i want to jump to the next position
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
             completion = {
-                completeopt = "menu,menuone,noinsert"
-            }
+                completeopt = "menu,menuone,noinsert",
+            },
+            experimental = {
+                ghost_text = false,
+            },
         })
     end,
 }
