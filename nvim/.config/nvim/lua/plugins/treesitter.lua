a196602687689825ab56d003d7feef679b87b05f
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-03-22 15:39:40 +0100

    nvim: chase treesitter-nvim rewrite changes
    This may require removing and reinstalling the plugin, to get the new
    main branch in.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -42,8 +42,5 @@ return {
                 enable = true,
             },
         },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-        end,
     },
 }
