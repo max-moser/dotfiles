50ed9222df36d3816f3baf6ab8af9f8a26d003ac
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-03-22 15:23:55 +0100

    nvim: overhaul colorscheme plugin definition
    Add more color schemes and set kanagawa as default.
    The default can be overridden via an environment variable, e.g.
    `NVIM_COLORSCHEME=catppuccin nvim`
    
    In the colorschemes table, use lazy.nvim plugin specs instead of strings
    as values, to better support color schemes that can take configuration
    options during initialization.
    
    For setting the default color scheme, set the `init` function instead of
    `config`. Simply overriding the latter without also implementing the
    default behavior causes the `opts` not being passed to the
    `plugin.setup()`. This effectively ignored the `opts` for the default
    color scheme.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,45 @@
 -- define a list of color schemes to load
-local default_colorscheme = "catppuccin"
-local colorschemes = {
-    afterglow = "danilo-augusto/vim-afterglow",
-    gruvbox = "ellisonleao/gruvbox.nvim",
-    deus = "ajmwagar/vim-deus",
-    nord = "shaunsingh/nord.nvim",
-    catppuccin = "catppuccin/nvim",
-    caret = "projekt0n/caret.nvim",
-}
+local default_colorscheme = vim.env.NVIM_COLORSCHEME or "kanagawa"
 
-local plugins = {
-    {
-        -- the main color scheme needs to be loaded first
-        colorschemes[default_colorscheme],
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme(default_colorscheme)
-        end,
+local colorschemes = {
+    afterglow = { "danilo-augusto/vim-afterglow" },
+    gruvbox = { "ellisonleao/gruvbox.nvim" },
+    deus = { "ajmwagar/vim-deus" },
+    nord = { "shaunsingh/nord.nvim" },
+    nordic = { "AlexvZyl/nordic.nvim" },
+    caret = { "projekt0n/caret.nvim" },
+    catppuccin = {
+        "catppuccin/nvim",
+        default = "catppuccin-nvim",
+    },
+    kanagawa = {
+        "rebelot/kanagawa.nvim",
+        opts = { transparent = false },
+    },
+    jellybeans = {
+        "WTFox/jellybeans.nvim",
+        opts = { transparent = false },
+        default = "jellybeans-muted",
     },
 }
 
--- add the above defined color schemes to the list of plugins
+-- set the default entry to be non-lazy, high-priority, and set an init function
+-- note: overriding the "config" function prevents the default "opts" mechanism
+local default_entry = colorschemes[default_colorscheme]
+default_entry.lazy = false
+default_entry.priority = 1000
+default_entry.init = function()
+    vim.cmd.colorscheme(default_entry.default or default_colorscheme)
+end
+local plugins = { default_entry }
+
+-- use only the package spec values for other color schemes, remove the keys
 colorschemes[default_colorscheme] = nil
-for _, repo in pairs(colorschemes) do
-    local entry = {
-        repo,
-        lazy = true,
-    }
-    table.insert(plugins, 1, entry)
+for _, config in pairs(colorschemes) do
+    if config.lazy == nil then
+        config.lazy = true
+    end
+    table.insert(plugins, 1, config)
 end
 
 return plugins
