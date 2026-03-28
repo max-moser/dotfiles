edba0821c4081d435260d0c8b8b2fa563852e06d
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-03-29 00:59:06 +0100

    nvim: update nvim-treesitter to main branch and add other plugins
    The new main branch for `nvim-treesitter` has reworked the entire
    plugin, along with its goals and overall feature set.
    Instead of a sort of framework for treesitter in neovim like before,
    it is now more of a parser installer plugin.
    
    Some of the functionality is now in neovim itself; other functionality
    is now part of other plugins.
    
    Added the `nvim-treesitter-context` plugin which shows the current
    "context" (e.g. classes, functions, ...) on the top of the screen.
    Added the `nvim-treesitter-textobjects` plugin which enhances the
    notion of text objects in neovim with the power of treesitter, improving
    navigation between code constructs like classes, functions, loops, etc.
    Removed the incremental selection feature which I've never really used.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,46 +1,145 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
         build = ":TSUpdate",
         event = { "BufReadPost", "BufNewFile" },
-        opts = {
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
+        init = function()
+            -- register an autocommand for automatically starting treesitter for supported file types
+            vim.api.nvim_create_autocmd("FileType", {
+                desc = "Start Treesitter when using supported file types",
+                callback = function(ev)
+                    local ts = require("nvim-treesitter")
+                    local filetype = ev.match
 
-                disable = function(lang, buf)
-                    -- i prefer the built-in highlighting of 'git rebase' commands
-                    if lang == "git_rebase" then
-                        return true
-                    end
+                    if vim.list_contains(ts.get_installed(), filetype) then
+                        -- if the file is larger than 5 MiB, treesitter starts becoming too slow for nice UX
+                        -- with more than 30 MiB, it starts becoming painful
+                        local max_filesize = 5 * 1024 * 1024
+                        local bytes = vim.fn.wordcount()["bytes"]
 
-                    -- if the file is larger than 5 MiB, treesitter starts becoming too slow for nice UX
-                    -- with more than 30 MiB, it starts becoming painful
-                    --
-                    -- treesitter-based folds start causing some pain much earlier (~1.5 MiB)
-                    local max_filesize = 5 * 1024 * 1024
-                    local max_filesize_folds = 3 * 512 * 1024
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size then
-                        if stats.size > max_filesize then
-                            -- disable if the file is too large
-                            return true
-                        elseif vim.opt.foldexpr:get() == "0" and stats.size < max_filesize_folds then
-                            -- if the file is small enough, enable treesitter-based folds
-                            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+                        if bytes <= max_filesize then
+                            if vim.opt.foldexpr:get() == "0" then
+                                vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                                vim.wo.foldmethod = "expr"
+                            end
+
+                            vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+                            vim.treesitter.start()
+
+                            -- load the other treesitter plugins
+                            require("nvim-treesitter-textobjects")
+                            require("treesitter-context")
+                        else
+                            vim.treesitter.stop()
                         end
+                    else
+                        vim.treesitter.stop()
                     end
-
-                    return false
                 end,
+            })
+        end,
+    },
+    {
+        -- enhances selections and motions like [m with the power of treesitter
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        lazy = true,
+        opts = {
+            move = {
+                set_jumps = true,
             },
-            incremental_selection = {
-                enable = true,
+            select = {
+                include_surrounding_whitespace = false,
             },
-            indent = {
-                enable = true,
-            },
+        },
+        init = function()
+            local select = require("nvim-treesitter-textobjects.select")
+            local move = require("nvim-treesitter-textobjects.move")
+            local function map(mode, key_combo, command, opts)
+                vim.keymap.set(mode, key_combo, command, opts)
+            end
+
+            -- hotkeys for selecting text objects
+            map({ "x", "o" }, "ac", function()
+                select.select_textobject("@class.outer", "textobjects")
+            end)
+            map({ "x", "o" }, "ic", function()
+                select.select_textobject("@class.inner", "textobjects")
+            end)
+            map({ "x", "o" }, "am", function()
+                select.select_textobject("@function.outer", "textobjects")
+            end)
+            map({ "x", "o" }, "im", function()
+                select.select_textobject("@function.inner", "textobjects")
+            end)
+            map({ "x", "o" }, "as", function()
+                select.select_textobject("@local.scope", "locals")
+            end)
+
+            -- hotkeys for jumping around text objects
+            -- classes
+            map({ "n", "x", "o" }, "[[", function()
+                move.goto_previous_start("@class.outer", "textobjects")
+            end)
+            map({ "n", "x", "o" }, "]]", function()
+                move.goto_next_start("@class.outer", "textobjects")
+            end)
+            map({ "n", "x", "o" }, "[]", function()
+                move.goto_previous_end("@class.outer", "textobjects")
+            end)
+            map({ "n", "x", "o" }, "][", function()
+                move.goto_next_end("@class.outer", "textobjects")
+            end)
+
+            -- functions
+            map({ "n", "x", "o" }, "[m", function()
+                move.goto_previous_start("@function.outer", "textobjects")
+            end)
+            map({ "n", "x", "o" }, "]m", function()
+                move.goto_next_start("@function.outer", "textobjects")
+            end)
+            map({ "n", "x", "o" }, "[M", function()
+                move.goto_previous_end("@function.outer", "textobjects")
+            end)
+            map({ "n", "x", "o" }, "]M", function()
+                move.goto_next_end("@function.outer", "textobjects")
+            end)
+
+            -- if statements: jump to start or end, whichever is closer
+            map({ "n", "x", "o" }, "]d", function()
+                move.goto_next("@conditional.outer", "textobjects")
+            end)
+            map({ "n", "x", "o" }, "[d", function()
+                move.goto_previous("@conditional.outer", "textobjects")
+            end)
+
+            -- loops
+            map({ "n", "x", "o" }, "[o", function()
+                move.goto_previous_start({ "@loop.inner", "@loop.outer" }, "textobjects")
+            end)
+            map({ "n", "x", "o" }, "]o", function()
+                move.goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects")
+            end)
+
+            -- scopes
+            map({ "n", "x", "o" }, "[s", function()
+                move.goto_previous_start("@local.scope", "locals")
+            end)
+            map({ "n", "x", "o" }, "]s", function()
+                move.goto_next_start("@local.scope", "locals")
+            end)
+        end,
+    },
+    {
+        -- show context of the currently selected line (e.g. class, function) on the top of the screen
+        "nvim-treesitter/nvim-treesitter-context",
+        lazy = true,
+        opts = {
+            enable = true,
+            multiwindow = false,
+            line_numbers = false,
+            separator = "▓",
         },
     },
 }
