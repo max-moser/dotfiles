4ea6e9785c62e70488f8f813d0a1b10828ae80e1
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-04-03 11:12:48 +0200

    nvim: replace completion plugins with built-in functionality
    Since nvim-lsp-compl has been ported to neovim core, having a basic
    autocompletion setup does not require plugins anymore.

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -46,20 +46,105 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 
 -- set up some hotkeys that require a running LSP to work
-vim.api.nvim_create_autocmd("LspAttach", {
-    desc = "LSP setup",
-    callback = function(event)
-        local opts = { buffer = event.buf }
-        local map = function(mode, keys, func, desc)
-            vim.keymap.set(mode, keys, func, { buffer = opts.buffer, desc = "LSP: " .. desc })
-        end
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--     desc = "LSP setup",
+--     callback = function(event)
+--         local opts = { buffer = event.buf }
+--         local map = function(mode, keys, func, desc)
+--             vim.keymap.set(mode, keys, func, { buffer = opts.buffer, desc = "LSP: " .. desc })
+--         end
+--
+--         -- utilize telescope to improve code navigation hotkeys
+--         map("n", "gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+--         map("n", "gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+--         map("i", "<C-S-Space>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help")
+--         map("n", "<F2>", "<Cmd>lua vim.lsp.buf.rename()<CR>", "Rename symbol")
+--         map({ "n", "x" }, "<F3>", "<Cmd>lua vim.lsp.buf.format({async = true})<CR>", "Format code")
+--         map("n", "<F4>", "<Cmd>lua vim.lsp.buf.code_action()<CR>", "Code action")
+--     end,
+-- })
 
-        -- utilize telescope to improve code navigation hotkeys
-        map("n", "gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-        map("n", "gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        map("i", "<C-S-Space>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help")
-        map("n", "<F2>", "<Cmd>lua vim.lsp.buf.rename()<CR>", "Rename symbol")
-        map({ "n", "x" }, "<F3>", "<Cmd>lua vim.lsp.buf.format({async = true})<CR>", "Format code")
-        map("n", "<F4>", "<Cmd>lua vim.lsp.buf.code_action()<CR>", "Code action")
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client ~= nil then
+            local function map(mode, keys, func, desc)
+                vim.keymap.set(mode, keys, func, { buffer = ev.buf, desc = "LSP: " .. desc })
+            end
+
+            if client:supports_method("textDocument/completion") then
+                -- trigger autocomplete on every keypress
+                local chars = {}
+                for i = 32, 126 do
+                    table.insert(chars, string.char(i))
+                end
+                client.server_capabilities.completionProvider.triggerCharacters = chars
+
+                -- enable completion
+                vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+
+                -- map ^SPACE to manually trigger completion
+                vim.keymap.set("i", "<C-Space>", function()
+                    vim.lsp.completion.get()
+
+                    -- TODO: this doesn't quite work, probably needs to go into an autocmd & have its :
+                    -- e.g. vim.bo[args.buf].formatexpr = nil
+                    -- local info = vim.fn.complete_info()
+                    -- local pum_visible = info.pum_visible
+                    -- local bufnr = info.preview_bufnr
+                    -- if pum_visible == 1 and bufnr ~= nil then
+                    --     vim.keymap.set("i", "<C-d>", "<C-e>", { buffer = bufnr, desc = "LSP: scroll docs" })
+                    -- end
+                end)
+
+                -- auto-select first entry for quick tab but don't insert it as text yet
+                -- vim.cmd[[set completeopt-=noselect]]
+                -- vim.cmd[[set completeopt+=noinsert]]
+
+                -- set some keymaps for the completion menu
+                vim.keymap.set("i", "<Tab>", function()
+                    return vim.fn.pumvisible() == 1 and "<C-y>" or "<Tab>"
+                end, { expr = true })
+                vim.keymap.set("i", "<C-j>", function()
+                    return vim.fn.pumvisible() == 1 and "<C-n>" or "<C-j>"
+                end, { expr = true })
+                vim.keymap.set("i", "<C-k>", function()
+                    return vim.fn.pumvisible() == 1 and "<C-p>" or "<C-j>"
+                end, { expr = true })
+                vim.keymap.set("i", "<C-Enter>", function()
+                    return vim.fn.pumvisible() == 1 and "<C-o>o" or "<C-Enter>"
+                end, { expr = true })
+
+                -- :h compl-autocomplete
+                -- TODO: discard snippet mode as soon as insert mode is left
+                -- TODO: window border around completion popup docs
+                --       [pumborder seems to affect the wrong thing :(]
+                -- TODO: hotkeys to scroll in the popup docs
+                --       [preview_bufnr can be obtained from complete_info(). Then you can create buffer-local keymaps for that buffer to handle scrolling.]
+                -- TODO: exclude Text suggestions { => is this even a real issue? they don't seem to pop up in *actual* LSP suggestions, only if there are no proper suggestions }
+                --
+                -- DONE: show documentation of hovered item [works with cot+=popup in 0.12+]
+                -- DONE: more keymaps (accept, abort, ...)
+                -- DONE: disable completions in certain contexts (e.g. comments)
+                --       [disabled in strings, enabled in comments but ok with cot+=noselect]
+            end
+
+            if client:supports_method("textDocument/formatting") then
+                map({ "n", "x" }, "<F3>", "<Cmd>lua vim.lsp.buf.format({async = true})<CR>", "Format code")
+            end
+
+            -- prefer LSP folding (over treesitter) if the client supports it
+            if client:supports_method("textDocument/foldingRange") then
+                local win = vim.api.nvim_get_current_win()
+                vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+            end
+
+            -- utilize telescope to improve code navigation hotkeys
+            map("n", "gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+            map("n", "gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+            map("i", "<C-S-Space>", vim.lsp.buf.signature_help, "Signature Help")
+            map("n", "<F2>", vim.lsp.buf.rename, "Rename symbol")
+            map("n", "<F4>", vim.lsp.buf.code_action, "Code action")
+        end
     end,
 })
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,87 +17,99 @@ local function setup_mason()
     })
 end
 
-local function setup_cmp()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-
-    -- confirm current autocomplete suggestion, expand or jump in the current snippet,
-    -- or perform the fallback action
-    local function confirm_cmp_or_jump(fallback)
-        if cmp.visible() then
-            -- if a suggestion is currently open, accept it
-            cmp.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace })
-        elseif luasnip.expand_or_jumpable() then
-            -- if still in a snippet, jump to the next position
-            luasnip.expand_or_jump()
-        else
-            fallback()
-        end
-    end
-
-    -- jump backwards if possible, or perform the fallback action
-    local function jump_backwards(fallback)
-        if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-        else
-            fallback()
-        end
-    end
-
-    cmp.setup({
-        enabled = function()
-            -- disable in 'prompt-buffer' (typically input for jobs)
-            local disabled = (vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt")
-
-            -- disable when recording/executing recordings
-            disabled = disabled or (vim.fn.reg_recording() ~= "")
-            disabled = disabled or (vim.fn.reg_executing() ~= "")
-
-            -- disable inside comments & strings
-            disabled = disabled or require("cmp.config.context").in_treesitter_capture("comment")
-            disabled = disabled or require("cmp.config.context").in_treesitter_capture("string_content")
-            return not disabled
-        end,
-        snippet = {
-            expand = function(args)
-                luasnip.lsp_expand(args.body)
-            end,
-        },
-        mapping = cmp.mapping.preset.insert({
-            -- `Ctrl-Space` to trigger completion menu
-            ["<C-Space>"] = cmp.mapping.complete(),
-
-            -- select next/prev item with ^j and ^k
-            ["<C-k>"] = cmp.mapping.select_prev_item(),
-            ["<C-j>"] = cmp.mapping.select_next_item(),
-
-            -- scrolling the docs
-            ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-d>"] = cmp.mapping.scroll_docs(4),
-            ["<C-S-k>"] = cmp.mapping.scroll_docs(-1),
-            ["<C-S-j>"] = cmp.mapping.scroll_docs(1),
-
-            -- allow `Enter` and `Tab` to confirm completion,
-            -- but also use `Tab` (and `Shift-Tab`) for cycling through snippet spots
-            ["<CR>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }),
-            ["<Tab>"] = cmp.mapping(confirm_cmp_or_jump, { "i", "s" }),
-            ["<S-Tab>"] = cmp.mapping(jump_backwards, { "i", "s" }),
-        }),
-        completion = {
-            completeopt = "menu,menuone,noinsert",
-            keyword_length = 2,
-        },
-        experimental = {
-            ghost_text = false,
-        },
-        sources = {
-            -- see here for a curated list of sources:
-            -- https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
-            { name = "nvim_lsp" },
-            { name = "luasnip" },
-        },
-    })
-end
+-- local function setup_cmp()
+--     local cmp = require("cmp")
+--     local luasnip = require("luasnip")
+--
+--     -- confirm current autocomplete suggestion, expand or jump in the current snippet,
+--     -- or perform the fallback action
+--     local function confirm_cmp_or_jump(fallback)
+--         if cmp.visible() then
+--             -- if a suggestion is currently open, accept it
+--             cmp.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace })
+--         elseif luasnip.expand_or_jumpable() then
+--             -- if still in a snippet, jump to the next position
+--             luasnip.expand_or_jump()
+--         else
+--             fallback()
+--         end
+--     end
+--
+--     -- jump backwards if possible, or perform the fallback action
+--     local function jump_backwards(fallback)
+--         if luasnip.jumpable(-1) then
+--             luasnip.jump(-1)
+--         else
+--             fallback()
+--         end
+--     end
+--
+--     -- cancel the completion if it's active
+--     local function cancel_completion(fallback)
+--         if cmp.visible() then
+--             cmp.close()
+--         else
+--             fallback()
+--         end
+--     end
+--
+--     cmp.setup({
+--         enabled = function()
+--             -- disable in 'prompt-buffer' (typically input for jobs)
+--             local disabled = (vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt")
+--
+--             -- disable when recording/executing recordings
+--             disabled = disabled or (vim.fn.reg_recording() ~= "")
+--             disabled = disabled or (vim.fn.reg_executing() ~= "")
+--
+--             -- disable inside comments & strings
+--             disabled = disabled or require("cmp.config.context").in_treesitter_capture("comment")
+--             disabled = disabled or require("cmp.config.context").in_treesitter_capture("string_content")
+--             return not disabled
+--         end,
+--         snippet = {
+--             expand = function(args)
+--                 luasnip.lsp_expand(args.body)
+--             end,
+--         },
+--         mapping = cmp.mapping.preset.insert({
+--             -- `Ctrl-Space` to trigger completion menu
+--             ["<C-Space>"] = cmp.mapping.complete(),
+--
+--             -- select next/prev item with ^j and ^k
+--             ["<C-k>"] = cmp.mapping.select_prev_item(),
+--             ["<C-j>"] = cmp.mapping.select_next_item(),
+--
+--             -- scrolling the docs
+--             ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+--             ["<C-d>"] = cmp.mapping.scroll_docs(4),
+--             ["<C-S-k>"] = cmp.mapping.scroll_docs(-1),
+--             ["<C-S-j>"] = cmp.mapping.scroll_docs(1),
+--
+--             -- allow `Enter` and `Tab` to confirm completion,
+--             -- but also use `Tab` (and `Shift-Tab`) for cycling through snippet spots
+--             ["<CR>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }),
+--             ["<Tab>"] = cmp.mapping(confirm_cmp_or_jump, { "i", "s" }), -- TODO this may need tweaking, currently there's no hotkey for jumping but not confirming
+--             ["<S-Tab>"] = cmp.mapping(jump_backwards, { "i", "s" }),
+--
+--             -- ^c to cancel the completion without entering normal mode
+--             ["<C-c>"] = cmp.mapping(cancel_completion, { "i", "s" }),
+--         }),
+--         completion = {
+--             completeopt = "menu,menuone,noinsert",
+--             keyword_length = 2,
+--         },
+--         experimental = {
+--             ghost_text = false,
+--         },
+--         sources = {
+--             -- see here for a curated list of sources:
+--             -- https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
+--             { name = "nvim_lsp" },
+--             { name = "luasnip" },
+--         },
+--     })
+-- end
 
 return {
     -- default LSP configurations
@@ -118,15 +130,15 @@ return {
     },
 
     -- autocompletion
-    {
-        "hrsh7th/nvim-cmp",
-        init = setup_cmp,
-    },
-    {
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    {
-        "L3MON4D3/LuaSnip",
-        build = "make install_jsregexp",
-    },
+    -- {
+    --     "hrsh7th/nvim-cmp",
+    --     init = setup_cmp,
+    -- },
+    -- {
+    --     "hrsh7th/cmp-nvim-lsp",
+    -- },
+    -- {
+    --     "L3MON4D3/LuaSnip",
+    --     build = "make install_jsregexp",
+    -- },
 }
