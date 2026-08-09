3dd415ea5fb820cbb175ea7c7c7b05b30928aab3
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-08-09 19:02:19 +0200

    nvim: disable editorconfig integration for diffs
    This includes hunk editing in git, e.g. for `git add -p`.
    
    Trailing whitespaces can be relevant for edited hunks to apply
    successfully.
    Having `trim_trailing_whitespace = true` set in `.editorconfig` can
    interfere with that, so we disable the editorconfig integration for
    `diff` files.

---
+++ nvim/.config/nvim/after/ftplugin/diff.lua
@@ -0,0 +1,7 @@
+-- ------------- --
+-- diff settings --
+-- ------------- --
+
+-- ignore ".editorconfig" for diffs
+-- e.g. because removing trailing whitespaces may break hunk editing in git
+vim.b.editorconfig = false
